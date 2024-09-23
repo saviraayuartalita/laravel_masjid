0185da6f52d37ba65b4c589e9c2ed86aec5bc790
saviraayuartalita <saviaayuartalita3101@gmail.com>
2024-09-23 14:07:01 +0700

    Delete storage/framework/views directory

--- storage/framework/views/1fbfe41ef8918fd098b1a68572981f69.php
+++
@@ -1,3 +0,0 @@
-<div class="p-8 bg-stone-600 text-lg">
-    <li class="text-white text-md">Dashboard Admin</li>
-</div><?php /**PATH C:\xampp\htdocs\masjid\resources\views/components/dashboard-header.blade.php ENDPATH**/ ?>
